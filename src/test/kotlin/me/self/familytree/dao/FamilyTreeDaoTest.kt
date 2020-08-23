f634e49196ae98de1e8293bc5c83619ca77d1b53
PeiqingMa <peiqing.ma@eventbank.com>
2020-08-23 11:24:59 +0800

    use embedded neo4j in test

--- src/test/kotlin/me/self/familytree/dao/FamilyTreeDaoTest.kt
+++ src/test/kotlin/me/self/familytree/dao/FamilyTreeDaoTest.kt
@@ -11,23 +11,13 @@ class FamilyTreeDaoTest(private val familyTreeDao: FamilyTreeDao) {
 
     @Test
     fun testCreatePerson() {
-        val name = "Axee Bces"
+        val name = "Axee Bces ${System.currentTimeMillis()}"
         val person = Person()
         person.addName(name)
         person.bioGender = Gender.Male
         val saved = familyTreeDao.upsertPerson(person)
         assertNotNull(saved?.id)
         assertTrue(saved?.names?.firstOrNull() == name)
-        val another = Person()
-        val anotherName = "Another Name"
-        another.addName(anotherName)
-        another.bioGender = Gender.Female
-        person.bioMother = another
-        person.addParent(another)
-        person.id = saved?.id
-        val updated = familyTreeDao.upsertPerson(person)
-        assertNotNull(updated?.bioMother)
-        assertTrue(updated?.bioMother?.names?.firstOrNull() == anotherName)
     }
 
 }
